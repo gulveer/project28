957891f1d4fae81c533ad48d06d7d6d6b6136aef
gulveer <72923744+gulveer@users.noreply.github.com>
2020-12-28 16:00:49 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,38 +1,23 @@
 
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
-var tree1;
-function preload()
-{
-	
-}
 
-function setup() {
-	createCanvas(1600, 700);
+const Engine = Matter.Engine; 
+const World = Matter.World; 
+const Bodies = Matter.Bodies; 
+var tree1; 
 
-
-	engine = Engine.create();
-	world = engine.world;
-
+function setup() { 
+	createCanvas(1600, 700); 
+	engine = Engine.create(); 
+	world = engine.world; 
 	//Create the Bodies Here.
- tree1 = new Tree(1200,650,20,600);
-
-	Engine.run(engine);
-  
-}
-
-
-function draw() {
-  rectMode(CENTER);
-  background(0);
-  
- 
-  tree1.display();
-  drawSprites();
- 
-}
-
-
+	 
+	tree1 = new Tree(1000,300,400,500); 
+	 
+	 Engine.run(engine); 
+	} 
+	
+	function draw() { 
+		background(0); 
+		tree1.display();
+	 }
 
--- tree.js
+++ tree.js
@@ -1,22 +1,23 @@
-class Tree {
-    constructor(x,y,width,height) {
-      var options = {
-          isStatic: true
-      }
-      this.body = Bodies.rectangle(x,y,width,height,options);
-      this.width = width;
-      this.height = height;
-      this.image = loadImage("sprite/tree.png");
-      World.add(world, this.body);
-    }
-    display(){
-      var pos =this.body.position;
-     push();
-      rectMode(CENTER);
-      fill("white");
-      rect(pos.x, pos.y, this.width, this.height);
-      imageMode(CENTER);
-      image(this.image, 0, 0, this.width, this.height);
-     pop();
-    }
-  };
+   class Tree { 
+    constructor(x,y,width,height) { 
+    var options = { 
+      isStatic: true 
+    } 
+    this.image = loadImage("sprite/tree.png"); 
+    this.body = Bodies.rectangle(x,y,this.width,this.height,options); 
+    this.width = width; 
+    this.height = height; 
+    
+    World.add(world, this.body); } 
+    
+    display(){ 
+      var pos =this.body.position; 
+     
+      push(); 
+      rectMode(CENTER); 
+      fill("white"); 
+      imageMode(CENTER); 
+      image(this.image, pos.x, pos.y, this.width, this.height); 
+      pop(); 
+    } 
+  }; 
